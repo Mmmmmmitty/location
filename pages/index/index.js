7e66ffcce107ad12893275ef9bd50e1bc1728137
liuwei <408057879@qq.com>
2018-12-16 20:06:19 +0800

    first conmmit

---
+++ pages/index/index.js
@@ -0,0 +1,94 @@
+//index.js
+//获取应用实例
+const app = getApp()
+var QQMapWX = require('../../utils/qqmap-wx-jssdk.js');
+var qqmapsdk;
+Page({
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    search_val: '',
+    list:[],
+    lat:'',
+    lng:''
+  },
+  inputVal(e) {
+    console.log(e.detail.value)
+    this.setData({
+      search_val: e.detail.value
+    })
+    this.getAddressList(e.detail.value)
+  },
+  // 选择搜索出来的地址
+  onClick(e){
+    console.log(e.currentTarget.dataset.item)
+    var item = e.currentTarget.dataset.item
+    this.setData({
+      lat: item.location.lat,
+      lng: item.location.lng,
+      place_title: item.title
+    })
+  },
+  // 根据输入地址请求地址列表
+  getAddressList(val) {
+    var self = this
+    qqmapsdk.search({
+      keyword: val,
+      success: function(res) {
+        console.log(res);
+        if (res.status == 0) {
+          self.setData({
+            list: res.data
+          })
+        }
+      },
+      fail: function(res) {
+        console.log(res);
+      },
+      complete: function(res) {
+        console.log(res);
+      }
+    });
+  },
+  onLoad: function() {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+    // 实例化API核心类
+    qqmapsdk = new QQMapWX({
+      key: 'VTZBZ-FAIWK-6DIJU-A2VHC-FW3MK-OQBCA'
+    });
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
---
+++ pages/map/map.js
@@ -0,0 +1,83 @@
+// pages/map/map.js
+var QQMapWX = require('../../utils/qqmap-wx-jssdk.js');
+var qqmapsdk;
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    // 实例化API核心类
+    qqmapsdk = new QQMapWX({
+      key: 'VTZBZ-FAIWK-6DIJU-A2VHC-FW3MK-OQBCA'
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+    // 调用接口
+    qqmapsdk.search({
+      keyword: '酒店',
+      success: function(res) {
+        console.log(res);
+      },
+      fail: function(res) {
+        console.log(res);
+      },
+      complete: function(res) {
+        console.log(res);
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
